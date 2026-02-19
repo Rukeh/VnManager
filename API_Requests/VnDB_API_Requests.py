1ab6508e0aa5d4dd08457fa3a1c496176e9a056b
lucas <lucasfourny2@gmail.com>
2026-02-19 17:09:32 +0100

    search function

---
+++ API_Requests/VnDB_API_Requests.py
@@ -0,0 +1,17 @@
+import requests
+
+def search_vns(title):
+    url = "https://api.vndb.org/kana/vn"
+    payload = {
+        "filters": ["search", "=", title],
+        "fields": "id, title, alttitle, released, languages, platforms, image.url, length, description, rating",
+        "results" : 10
+    }
+    r = requests.post(url = url, json = payload)
+    data = r.json()
+    return data["results"]
+
+search_test = input("What would you like to search ?")
+s_results = search_vns(search_test)
+for vn in s_results:
+    print(vn["title"], vn["id"], vn["image"])
