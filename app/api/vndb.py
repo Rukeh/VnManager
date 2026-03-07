07fed992059389e1a733f45e1e131ad9d6c75a36
lucas <lucasfourny2@gmail.com>
2026-03-07 01:07:23 +0100

    added compilation folders

--- app/api/vndb.py
+++ app/api/vndb.py
@@ -10,7 +10,7 @@ def search_vns(title):
     payload = {
         "filters": ["search", "=", title],
         "fields": "id, title, alttitle, released, languages, platforms, image.url, length, description, rating, tags.name, tags.rating, tags.spoiler, tags.lie, image.sexual",
-        "results": 50,
+        "results": 25,
     }
     response = requests.post(url=url, json=payload)
     response.raise_for_status()
