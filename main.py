533168939df915479fc2c6fe645037d5fd96a013
Rukeh <lucasfourny2@gmail.com>
2026-03-24 16:23:37 +0100

    enables the theme on startup by reading into the save (if not returns default theme which is currently pink)

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from app.ui.main.main_window import run
+from app.utils.save import load_data
+from app.ui.shared.theme import set_active_theme, DEFAULT_THEME_NAME
 
 if __name__ == "__main__":
+    data = load_data()
+    theme_name = data.get("settings", {}).get("theme_name", DEFAULT_THEME_NAME)
+    try:
+        set_active_theme(theme_name)
+    except ValueError:
+        set_active_theme(DEFAULT_THEME_NAME)
+    from app.ui.main.main_window import run
     run()
 #do not touch
-
